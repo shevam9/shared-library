2a26f5debe813676030c55a401a812b8b43b9803
shevam9 <152986112+shevam9@users.noreply.github.com>
2024-01-14 15:19:55 +0530

    Update Clone.groovy

--- vars/Clone.groovy
+++ vars/Clone.groovy
@@ -1,6 +1,6 @@
-def call() {
  def gitRepoUrl = 'https://github.com/opstree/spring3hibernate.git'
 
     
     checkout([$class: 'GitSCM', branches: [[name: '*/java7']], userRemoteConfigs: [[url: gitRepoUrl]]])
+
 
