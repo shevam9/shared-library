774bb1f738ff56c8250bab834b247a43e9f20284
shevam9 <152986112+shevam9@users.noreply.github.com>
2024-01-14 15:02:48 +0530

    Update Clone.groovy

--- vars/Clone.groovy
+++ vars/Clone.groovy
@@ -1,14 +1,6 @@
-def call(String repoUrl, String branch = 'master') {
-    def repoDir = checkoutRepo(repoUrl, branch)
-    return repoDir
-}
+def call() {
+ def gitRepoUrl = 'https://github.com/opstree/spring3hibernate.git'
 
-def checkoutRepo(String repoUrl, String branch) {
-    node {
-        def repoDir = "${env.WORKSPACE}/my_repo"
-        checkDir()
-        checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], userRemoteConfigs: [[url: repoUrl, credentialsId: 'your_git_credentials']]])
-        return repoDir
-    }
-}
+    
+    checkout([$class: 'GitSCM', branches: [[name: '*/java7']], userRemoteConfigs: [[url: gitRepoUrl]]])
 
