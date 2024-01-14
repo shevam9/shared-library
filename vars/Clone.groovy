d581bdd1dcaf1302fe206ca91a2424c10d5841c7
shevam9 <152986112+shevam9@users.noreply.github.com>
2024-01-14 14:17:01 +0530

    Update Clone.groovy

--- vars/Clone.groovy
+++ vars/Clone.groovy
@@ -1,7 +1,14 @@
-def call(String repoUrl, String branch = 'main') {
-    def repoDir = "${env.WORKSPACE}/my_repo"
-    deleteDir()
-    checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], userRemoteConfigs: [[url: repoUrl, credentialsId: 'your_git_credentials']]])
+def call(String repoUrl, String branch = 'master') {
+    def repoDir = checkoutRepo(repoUrl, branch)
     return repoDir
+}
+
+def checkoutRepo(String repoUrl, String branch) {
+    node {
+        def repoDir = "${env.WORKSPACE}/my_repo"
+        deleteDir()
+        checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], userRemoteConfigs: [[url: repoUrl, credentialsId: 'your_git_credentials']]])
+        return repoDir
+    }
 }
 
