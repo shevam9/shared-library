ccb488ce15eae8c6f9545ef0a102d40bc8e8db87
shevam9 <152986112+shevam9@users.noreply.github.com>
2024-01-14 14:14:12 +0530

    Update Clone.groovy

--- vars/Clone.groovy
+++ vars/Clone.groovy
@@ -1,5 +1,5 @@
-def call(String repoUrl, String branch = 'master') {
-    def repoDir = "${WORKSPACE}/my_repo"
+def call(String repoUrl, String branch = 'main') {
+    def repoDir = "${env.WORKSPACE}/my_repo"
     deleteDir()
     checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], userRemoteConfigs: [[url: repoUrl, credentialsId: 'your_git_credentials']]])
     return repoDir
